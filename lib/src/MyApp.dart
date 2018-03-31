70a855703cf819febcf56b878d80397e085b79d4
Andrious <Andrious@users.noreply.github.com>
2018-03-31 12:23:50 -0400

    Add Model as a parameter

--- lib/src/MyApp.dart
+++ lib/src/MyApp.dart
@@ -1,10 +1,11 @@
 import 'package:mvc/App.dart' show App;
 import 'View.dart' show View;
 import 'Controller.dart' show Controller;
+import 'Model.dart' show Model;
 
 /// App Class
 ///
 class MyApp extends App{
 
-  MyApp(): super(new View(new Controller()));
+  MyApp(): super(new View(new Controller(new Model())));
 }
