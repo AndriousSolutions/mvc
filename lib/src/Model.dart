432d0326adca7d7a038961a4b80047d6d957c9ec
Andrious <Andrious@users.noreply.github.com>
2018-04-15 16:35:23 -0500

    added setCon()

--- lib/src/Model.dart
+++ lib/src/Model.dart
@@ -1,7 +1,16 @@
 import 'package:mvc/AppModel.dart' show AppModel;
+import 'Controller.dart';
 
 /// Model Class
 ///
 class Model extends AppModel {
 
+
+
+  setCon(Controller con){
+
+    _con = con;
+  }
+
+  Controller _con;
 }
