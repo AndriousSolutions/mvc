fc2ea330da72e90f9a5945501b05d3f5371d42d4
Andrious <Andrious@users.noreply.github.com>
2018-08-24 21:27:25 -0500

    abstract class StatedWidget

--- lib/MVC.dart
+++ lib/MVC.dart
@@ -14,75 +14,67 @@ import 'package:flutter/widgets.dart' show AppLifecycleState, BuildContext, Key,
 
 import 'package:flutter/material.dart' show AppLifecycleState, BuildContext, Key, RenderObject, State, StatefulWidget, VoidCallback, Widget, WidgetsBinding, WidgetsBindingObserver, mustCallSuper;
 
-import 'StatedWidget.dart';
-
-
 
 
 /// The State Object for this MVC Design Pattern
-class MVCState extends State<StatefulWidget> with WidgetsBindingObserver  {
-
+class MVController extends StatedWidgetState{
+  
   /// Allow for Mixins
-  MVCState(): super();
+  MVController(): super();
 
-
-  MVCState.setView(MCView vw){
-    set(vw);
+  
+  /// The View
+  @override
+  Widget build(BuildContext context){
+    /// Here's where the magic happens.
+    _build = _view.build(context);
+    return _build;
   }
 
-
-  MVCState.setCon(MVController con){
-    _con = con;
-    /// Get a reference of the State object for the Controller.
-    _con?._state = this;
-  }
-
-
-  MVCState set(MCView vw){
-    _view = vw;
-    _con = _view?._con;
-    /// Get a reference of the State object for the Controller.
-    _view?._con?._state = this;
-    return this;
-  }
-       
-
+  
   get view => _view;
   MCView _view;
-
-
-  MVController _con;
 
 
   get buildWidget => _build;
   Widget _build;
 
-
+  /// The framework will call this method exactly once.
+  /// Only when the [State] object is first created.
+  ///
+  /// Override this method to perform initialization that depends on the
+  /// location at which this object was inserted into the tree.
+  /// (i.e. Subscribe to another object it depends on during [initState],
+  /// unsubscribe object and subscribe to a new object when it changes in
+  /// [didUpdateWidget], and then unsubscribe from the object in [dispose].
   @override
   void initState(){
     /// called when the [State] object is first created.
     super.initState();
     _view?.widget = widget;
-    _con?.initState();
     WidgetsBinding.instance.addObserver(this);
   }
 
-
+  /// The framework calls this method whenever it removes this [State] object
+  /// from the tree. It might reinsert it into another part of the tree.
+  /// Subclasses should override this method to clean up any links between
+  /// this object and other elements in the tree (e.g. if you have provided an
+  /// ancestor with a pointer to a descendant's [RenderObject]).
   @override
   void deactivate(){
-    /// called when this [State] object is removed from the tree.
-    _con?.deactivate();
     super.deactivate();
   }
 
-
+  /// The framework calls this method when this [State] object will never
+  /// build again. The [State] object's lifecycle is terminated.
+  /// Subclasses should override this method to release any resources retained
+  /// by this object (e.g., stop any active animations).
   @override
   @mustCallSuper
   void dispose(){
     /// called when this [State] object will never build again.
     _build = null;
     WidgetsBinding.instance.removeObserver(this);
-    _con?.dispose();
     super.dispose();
   }
 
@@ -94,32 +86,23 @@ class MVCState extends State<StatefulWidget> with WidgetsBindingObserver  {
     /// The framework always calls [build] after calling [didUpdateWidget], which
     /// means any calls to [setState] in [didUpdateWidget] are redundant.
     super.didUpdateWidget(oldWidget);
-    _con?.didUpdateWidget(oldWidget);
   }
 
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     /// Passing either the values AppLifecycleState.paused or AppLifecycleState.resumed.
-    _con?.didChangeAppLifecycleState(state);
   }
 
 
-  void reState(fn) {
-    /// Calls the 'real' setState()
-    /// setState() can only be called within this class.
-    /// You'd get an error if not mounted.
-    if(mounted){
-      setState(fn);
-    }
+  void setState(fn){
+    /// Call the State object's setState() function.
+    super.setState(fn);
   }
 
-  
-  /// The View
-  Widget build(BuildContext context){
-    /// Here's where the magic happens.
-    _build = _view.build(context);
-    return _build;
+  void refresh(){
+    /// Refresh the Widget Tree Interface
+    setState(() {});
   }
 }
 
@@ -128,7 +111,9 @@ class MVCState extends State<StatefulWidget> with WidgetsBindingObserver  {
 /// The View of MVC
 abstract class MCView{
 
-  MCView([this._con]);
+  MCView([this._con]){
+    _con?._view = this;
+  }
 
   MVController _con;
 
@@ -142,15 +127,15 @@ abstract class MCView{
   }
 
   /// Allow for the widget getter in the build() function.
-  StatefulWidget _widget;
+  StatedWidget _widget;
   get widget => _widget ?? this;
-  set widget(StatefulWidget w) => _widget = w;
+  set widget(StatedWidget w) => _widget = w;
 
   /// BuildContext is always useful in the build() function.
-  get context => _con?._state?.context;
+  get context => _con?.context;
 
   /// Ensure the State Object is 'mounted' and not being terminated.
-  get mounted => _con?._state?.mounted;
+  get mounted => _con?.mounted;
 
   /// Provide the setState() function to the build() function.
   /// Although it's the Controller that should do all the calling of setState() function.
@@ -163,29 +148,92 @@ abstract class MCView{
 }
 
 
+abstract class StatedWidget extends StatefulWidget {
+  StatedWidget({ Key key }) : super(key: key);
 
 
-/// The Controller of MVC
-class MVController {
-  MVController([this._dataObj]){
-    _dataObj?.con = this;
+  /// This is where the magic happens.
+  Widget build(BuildContext context);
+
+
+  void initState(){
+
   }
 
-  StatedData _dataObj;
 
-  /// A reference to the State object.
-  /// Assigned only in this library file in the MVCState class
+  void deactivate(){
+
+  }
+
+
+  void dispose(){
+
+  }
+
+
+  void reassemble() {
+
+  }
+
+
+  void didUpdateWidget(StatefulWidget oldWidget) {
+
+  }
+
+
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+
+  }
+
+
+  void didChangeDependencies() {
+
+
+  }
+
+
   get state => _state;
-  MVCState _state;
+  final StatedWidgetState _state = StatedWidgetState();
 
-  /// Allow for the widget getter in the build() function.
-  get widget => _state?._view;
 
-  /// BuildContext is always useful in the build() function.
-  get context => _state?.context;
 
-  /// Ensure the State Object is 'mounted' and not being terminated.
-  get mounted => _state?.mounted;
+  @override
+  State createState(){
+    return _state;
+  }
+
+
+  get widget => _state.widget;
+
+
+  get context => _state.context;
+
+
+  get mounted => _state.mounted;
+
+
+  void setState(fn){
+    /// Call the State object's setState() function.
+    _state._reState(fn);
+  }
+
+
+  void refresh(){
+    /// Refresh the Widget Tree Interface
+    setState(() {});
+  }
+}
+
+
+
+class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
+
+
+  @override
+  Widget build(BuildContext context){
+    /// Here's where the magic happens.
+    return widget.build(context);
+  }
 
 
   /// The framework will call this method exactly once.
@@ -196,59 +244,80 @@ class MVController {
   /// (i.e. Subscribe to another object it depends on during [initState],
   /// unsubscribe object and subscribe to a new object when it changes in
   /// [didUpdateWidget], and then unsubscribe from the object in [dispose].
-  void initState() {
-    _dataObj?.initState();
+  @override
+  void initState(){
+    super.initState();
+    widget.initState();
   }
+
 
   /// The framework calls this method whenever it removes this [State] object
   /// from the tree. It might reinsert it into another part of the tree.
   /// Subclasses should override this method to clean up any links between
   /// this object and other elements in the tree (e.g. if you have provided an
   /// ancestor with a pointer to a descendant's [RenderObject]).
+  @override
   void deactivate(){
-    _dataObj?.deactivate();
+    widget.deactivate();
+    super.deactivate();
   }
+
 
   /// The framework calls this method when this [State] object will never
   /// build again. The [State] object's lifecycle is terminated.
   /// Subclasses should override this method to release any resources retained
   /// by this object (e.g., stop any active animations).
+  @override
+  @mustCallSuper
   void dispose(){
-    _dataObj?.dispose();
+    widget.dispose();
+    /// called when this [State] object will never build again.
+    super.dispose();
   }
 
-  void didUpdateWidget(StatefulWidget oldWidget) {
-    _dataObj?.didUpdateWidget(oldWidget);
-  }
 
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    _dataObj?.didChangeAppLifecycleState(state);
-  }
-
-  void setState(fn){
+  /// Notify the framework that the internal state of this object has changed.
+  void _reState(fn){
     /// Call the State object's setState() function.
-    _state?.reState(fn);
+    super.setState(fn);
   }
 
-  void refresh(){
-    /// Refresh the Widget Tree Interface
-    _state?.reState(() {});
+
+  /// Called whenever the application is reassembled during debugging, for
+  /// example during hot reload.
+  /// See also:
+  ///
+  /// * [BindingBase.reassembleApplication].
+  /// * [Image], which uses this to reload images.
+  @mustCallSuper
+  void reassemble() {
+    super.reassemble();
+    widget.reassemble();
   }
 
-  void padState(State state){
-    _states.add(state);
-    _state = state;
+  @override
+  void didUpdateWidget(StatefulWidget oldWidget) {
+    /// Override this method to respond when the [widget] changes (e.g., to start
+    /// implicit animations).
+    /// The framework always calls [build] after calling [didUpdateWidget], which
+    /// means any calls to [setState] in [didUpdateWidget] are redundant.
+    super.didUpdateWidget(oldWidget);
+    widget.didUpdateWidget(oldWidget);
   }
 
-  State popState(){
-    return _states.isEmpty ? _state : _states.remove(_state);
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    /// Passing either the values AppLifecycleState.paused or AppLifecycleState.resumed.
+    widget.didChangeAppLifecycleState(state);
   }
 
-  final _states = Set<State>();
+  /// Called when a dependency of this [State] object changes.
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.didChangeDependencies();
+  }
 }
-
-
-
 
 
 
