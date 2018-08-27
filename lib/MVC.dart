25506352439db1ecff62394f2d95717e94791de3
Andrious <Andrious@users.noreply.github.com>
2018-08-27 16:54:09 -0500

    class MVController extends State<StatefulWidget>

--- lib/MVC.dart
+++ lib/MVC.dart
@@ -10,14 +10,17 @@
 
 library mvc;
 
+import 'package:flutter/material.dart' show AppLifecycleState, BuildContext, GlobalKey, Image, Key, RenderObject, State, StatefulWidget, VoidCallback, Widget, WidgetsBinding, WidgetsBindingObserver, mustCallSuper;
+
 import 'package:flutter/widgets.dart' show AppLifecycleState, BuildContext, Key, RenderObject, State, StatefulWidget, VoidCallback, Widget, WidgetsBinding, WidgetsBindingObserver;
 
 import 'package:flutter/material.dart' show AppLifecycleState, BuildContext, Key, RenderObject, State, StatefulWidget, VoidCallback, Widget, WidgetsBinding, WidgetsBindingObserver, mustCallSuper;
 
+import 'package:flutter/foundation.dart' show BindingBase, DiagnosticPropertiesBuilder, Key, VoidCallback, mustCallSuper;
 
 
 /// The State Object for this MVC Design Pattern
-class MVController extends StatedWidgetState{
+class MVController extends State<StatefulWidget> with WidgetsBindingObserver{
   
   /// Allow for Mixins
   MVController(): super();
@@ -127,9 +130,9 @@ abstract class MCView{
   }
 
   /// Allow for the widget getter in the build() function.
-  StatedWidget _widget;
+  StatefulWidget _widget;
   get widget => _widget ?? this;
-  set widget(StatedWidget w) => _widget = w;
+  set widget(StatefulWidget w) => _widget = w;
 
   /// BuildContext is always useful in the build() function.
   get context => _con?.context;
@@ -148,75 +151,65 @@ abstract class MCView{
 }
 
 
-abstract class StatedWidget extends StatefulWidget {
-  StatedWidget({ Key key }) : super(key: key);
 
+
+abstract class StatedWidget {
+
+  
+  StatedWidget(){
+    _widget._state._statedWidget = this;
+  }
+
+  final WidgetStated _widget = WidgetStated(key: GlobalKey<StatedWidgetState>());
 
   /// This is where the magic happens.
   Widget build(BuildContext context);
-
+  
 
   void initState(){
 
   }
 
-
   void deactivate(){
 
   }
-
 
   void dispose(){
 
   }
 
-
   void reassemble() {
 
   }
-
 
   void didUpdateWidget(StatefulWidget oldWidget) {
 
   }
 
-
   void didChangeAppLifecycleState(AppLifecycleState state) {
 
   }
 
-
   void didChangeDependencies() {
 
-
   }
 
-
-  get state => _state;
-  final StatedWidgetState _state = StatedWidgetState();
-
-
-
-  @override
-  State createState(){
-    return _state;
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    _widget._state.debugFillProperties(properties);
   }
 
+  get state => _widget._state;
 
-  get widget => _state.widget;
+  get widget => _widget;
 
+  get context => _widget._state.context;
 
-  get context => _state.context;
-
-
-  get mounted => _state.mounted;
-
+  get mounted => _widget._state.mounted;
 
   void setState(fn){
     /// Call the State object's setState() function.
-    _state._reState(fn);
+    _widget._state._reState(fn);
   }
-
 
   void refresh(){
     /// Refresh the Widget Tree Interface
@@ -225,14 +218,26 @@ abstract class StatedWidget extends StatefulWidget {
 }
 
 
+class WidgetStated extends StatefulWidget {
+  WidgetStated({ Key key }) : super(key: key);
 
-class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
+  final StatedWidgetState _state = StatedWidgetState();
 
+  @override
+  State createState(){
+    return _state;
+  }
+}
+
+
+class StatedWidgetState extends State<WidgetStated> with WidgetsBindingObserver{
+
+  StatedWidget _statedWidget;
 
   @override
   Widget build(BuildContext context){
     /// Here's where the magic happens.
-    return widget.build(context);
+    return _statedWidget.build(context);
   }
 
 
@@ -247,7 +252,7 @@ class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
   @override
   void initState(){
     super.initState();
-    widget.initState();
+    _statedWidget.initState();
   }
 
 
@@ -258,7 +263,7 @@ class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
   /// ancestor with a pointer to a descendant's [RenderObject]).
   @override
   void deactivate(){
-    widget.deactivate();
+    _statedWidget.deactivate();
     super.deactivate();
   }
 
@@ -268,9 +273,8 @@ class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
   /// Subclasses should override this method to release any resources retained
   /// by this object (e.g., stop any active animations).
   @override
-  @mustCallSuper
   void dispose(){
-    widget.dispose();
+    _statedWidget.dispose();
     /// called when this [State] object will never build again.
     super.dispose();
   }
@@ -292,7 +296,7 @@ class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
   @mustCallSuper
   void reassemble() {
     super.reassemble();
-    widget.reassemble();
+    _statedWidget.reassemble();
   }
 
   @override
@@ -302,20 +306,20 @@ class StatedWidgetState extends State<StatedWidget> with WidgetsBindingObserver{
     /// The framework always calls [build] after calling [didUpdateWidget], which
     /// means any calls to [setState] in [didUpdateWidget] are redundant.
     super.didUpdateWidget(oldWidget);
-    widget.didUpdateWidget(oldWidget);
+    _statedWidget.didUpdateWidget(oldWidget);
   }
 
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     /// Passing either the values AppLifecycleState.paused or AppLifecycleState.resumed.
-    widget.didChangeAppLifecycleState(state);
+    _statedWidget.didChangeAppLifecycleState(state);
   }
 
   /// Called when a dependency of this [State] object changes.
   void didChangeDependencies() {
     super.didChangeDependencies();
-    widget.didChangeDependencies();
+    _statedWidget.didChangeDependencies();
   }
 }
 
